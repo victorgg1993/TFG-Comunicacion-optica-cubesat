ba967d58b9414f43b1918b123376d4cb6df2e416
lockpicker <vigomga1993@gmail.com>
2020-06-21 14:05:26 +0200

    añadido archivo con defines para limpiar un poco el código

---
+++ codigo_satelite/PPM/Receptor/Arduino/receptor/src/defines.h
@@ -0,0 +1,11 @@
+#ifndef _defines_h_
+#define _defines_h_
+
+#define BIT_START 0
+#define BIT_DATA 1
+#define BIT_STOP 4
+
+#define SIZE_BUFFER 5 // relacionado con el nº de paquetes que envía el ARM
+
+
+#endif
