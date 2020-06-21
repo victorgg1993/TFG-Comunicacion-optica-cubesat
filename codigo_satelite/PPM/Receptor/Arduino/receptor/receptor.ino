5704c5286cdb1c75ddfa6066684aa35fb340543b
lockpicker <vigomga1993@gmail.com>
2020-06-21 13:56:21 +0200

    se le ha implementado un LCD, muestra 2 datos por la pantalla, ya que por su tamaño no da para mucho más. En principio está todo terminado del receptor PPM

--- codigo_satelite/PPM/Receptor/Arduino/receptor/receptor.ino
+++ codigo_satelite/PPM/Receptor/Arduino/receptor/receptor.ino
@@ -5,26 +5,34 @@
   así que la resolución de arduino es de múltiplos de 4
   ( vamos, que salta de 4 en 4 ), hay que tenerlo en cuenta para ajustar
   el TIM4 del STM32
+
+  *****************> ARM, PB12 (PPM)      => Arduino, 3 (PPM) <*****************
 */
 
-// ARM, PB12 (PPM)      => Arduino, 3 (PPM)
-#define PIN_PPM 3
-#define BIT_START 0
-#define BIT_DATA 1
-#define BIT_STOP 4
+#include <LiquidCrystal.h>
+#include "src/defines.h" // aquí hay algunos defines usados en el código (bit stop y cosas así)
+#include "src/drivers.h" // aquí tenemos los pines y sus definiciones
+#include "src/TLE/TLE.h"
 
-#define SIZE_BUFFER 5 // relacionado con el nº de paquetes que envía el ARM
+// Variables
+volatile char paquetes[SIZE_LINES];
+volatile byte puntero = 0;
 
 volatile unsigned long tiempo = 0;
-volatile char paquetes[(SIZE_BUFFER)];
-volatile byte puntero = 0;
 volatile byte bit_start = BIT_START;
 volatile bool EOT = false;
+
+// Objetos
+LiquidCrystal lcd(PIN_RS, PIN_E, PIN_D4, PIN_D5, PIN_D6, PIN_D7);
 
 void setup()
 {
   Serial.begin(9600);
+  lcd.setCursor(0, 0);
   Serial.println("Init!");
+
+  lcd.begin(16, 2);
+  lcd.print("Esperando datos");
 
   pinMode(PIN_PPM, INPUT);
   attachInterrupt(digitalPinToInterrupt(PIN_PPM), ISR_pin_PPM, CHANGE); // cambio estado pin = ISR PPM
@@ -34,7 +42,28 @@ void loop()
 {
   if (EOT == true)
   {
-    imprimir_datos_recibidos();
+    imprimir_datos_recibidos(); // por la uart
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Nuevos datos!");
+    delay(1500);
+    lcd.clear();
+
+    lcd.setCursor(0, 0); // línea 1
+    lcd.print("Sat. c. n: ");
+    lcd.print(get_line(LINE1_CATALOG_NUM, paquetes));
+
+    lcd.setCursor(0, 1); // línea 2
+    lcd.print("Launch year: ");
+    lcd.print(get_line(LINE1_INTERN_DES_LAUNCH_YEAR, paquetes));
+
+    while(1); // temporal
+    delay(1000); // volvemos a empezar
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Esperando datos");
+
     reset_variables_ppm();
     EOT = false;
   }
