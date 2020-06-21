470d40de2996de4f7aaf11642162814e8652a281
lockpicker <vigomga1993@gmail.com>
2020-06-21 16:24:13 +0200

    creado receptor PWM, todavía no va perfecto, tiene un ofset de +1 char y hace que lo muestre todo desplazado

--- codigo_satelite/PWM/Receptor/Arduino/receptor/receptor.ino
+++ codigo_satelite/PWM/Receptor/Arduino/receptor/receptor.ino
@@ -6,23 +6,35 @@
   el TIM4 del STM32
 */
 
+#include <LiquidCrystal.h>
+#include "src/defines.h" // aquí hay algunos defines usados en el código (bit stop y cosas así)
+#include "src/drivers.h" // aquí tenemos los pines y sus definiciones
+#include "src/TLE/TLE.h"
+
 // ARM, PB12 (PWM)      => Arduino, 3 (PWM)
 #define NUM_CALIBRACION 2568
 
-#define PIN_PWM 3
-#define BIT_STOP 4
-#define SIZE_BUFFER 4 // relacionado con el nº de paquetes que envía el ARM
+// Variables
+volatile char paquetes[SIZE_LINES];
 
 volatile unsigned long tiempo = 0;
-volatile char paquetes[(SIZE_BUFFER)];
-
 volatile byte puntero = 0;
 volatile bool EOT = false;
+
+// Objetos
+LiquidCrystal lcd(PIN_RS, PIN_E, PIN_D4, PIN_D5, PIN_D6, PIN_D7);
 
 void setup()
 {
   Serial.begin(9600);
   Serial.println("Init!");
+
+  lcd.setCursor(0, 0);
+  lcd.begin(16, 2);
+  lcd.print("Esperando datos");
+  lcd.setCursor(0, 1);
+  lcd.print("Modo: PWM");
+
   pinMode(PIN_PWM, INPUT);
   attachInterrupt(digitalPinToInterrupt(PIN_PWM), ISR_pin_PWM, CHANGE); // cambio estado pin = ISR PWM
 }
@@ -31,6 +43,28 @@ void loop()
   if (EOT == true)
   {
     imprimir_datos_recibidos();
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Nuevos datos!");
+    delay(1500);
+    lcd.clear();
+
+    lcd.setCursor(0, 0); // línea 1
+    lcd.print("Sat. c. n: ");
+    lcd.print(get_line(LINE1_CATALOG_NUM, paquetes));
+
+    lcd.setCursor(0, 1); // línea 2
+    lcd.print("Launch year: ");
+    lcd.print(get_line(LINE1_INTERN_DES_LAUNCH_YEAR, paquetes));
+
+    while (1);   // temporal
+    delay(1000); // volvemos a empezar
+
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Esperando datos");
+
     reset_variables_pwm();
     EOT = false;
   }
@@ -45,7 +79,7 @@ void ISR_pin_PWM()
     break;
 
   case LOW:
-    unsigned long palabra = ((micros() - tiempo) / NUM_CALIBRACION );
+    unsigned long palabra = ((micros() - tiempo) / NUM_CALIBRACION);
 
     if (palabra == BIT_STOP) // if señal de stop => end of transmission
     {
@@ -71,7 +105,7 @@ void imprimir_datos_recibidos()
 {
   Serial.print("Data: ");
 
-  for (int i = 0; i < SIZE_BUFFER; i++)
+  for (int i = 0; i < strlen(paquetes); i++)
   {
     Serial.print(paquetes[i]);
   }
