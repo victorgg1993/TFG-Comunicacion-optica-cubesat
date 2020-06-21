a3801d20da0b7c7ce8033bf19c4b9e7ea91dcd24
lockpicker <vigomga1993@gmail.com>
2020-06-21 14:06:05 +0200

    añadido archivo con los pines de cada funcionalidad del arduino (lcd, ppm)

---
+++ codigo_satelite/PPM/Receptor/Arduino/receptor/src/drivers.h
@@ -0,0 +1,15 @@
+#ifndef _drivers_h_
+#define _drivers_h_
+
+// ARM, PB12 (PPM)      => Arduino, 3 (PPM)
+#define PIN_PPM 3
+
+// LCD
+#define PIN_RS 8  // RS
+#define PIN_E 9	  // E
+#define PIN_D4 A0 // D4
+#define PIN_D5 A1 // D5
+#define PIN_D6 A2 // D6
+#define PIN_D7 A3 // D7
+
+#endif
